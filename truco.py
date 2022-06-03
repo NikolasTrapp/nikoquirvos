fec25a30c153c4d0a6439817e2fe0966ea12a9c0
NikolasTrapp <nikolastrapp@gmail.com>
2022-06-02 22:16:17 -0300

    algumas melhorias

--- truco.py
+++ truco.py
@@ -99,10 +99,13 @@ class Truco:
         # definindo a maquina e a manilha
         self.maquina = choice([*self.cartas.keys()])
         self.manilha = self.manilhas[self.maquina]
+        print("="*60)
         print(f"a maquina é o {self.maquina}, a manilha é o {self.manilha}")
 
         #atualizando o peso da manilha
         self.cartas.update({self.manilha: {"paus": 14, "copas": 13, "espadas": 12, "moles": 11}})
+        '''for i in self.cartas:
+            print(self.cartas[i])'''
 
     def distribuir_cartas(self):
         #definir a manilha
@@ -143,12 +146,10 @@ class Truco:
         self.rodada += 1
 
     def jogar(self, jogadores: list):
-        fim = time()
-        print(fim)
         t1, t2 = 0, 0 #definir os pontos dos times
-        maior = 0 #definir a carta mais forte
         string_random = "(1, 2, 3)" #string para aparecer no bgl de pedir a carta
         for _ in range(1, 4): #numero de rounds da rodada
+            maior = 0 #definir a carta mais forte
             print("="*60) #print random
             '''print(*self.player1)
             print(*self.player2)
@@ -156,28 +157,34 @@ class Truco:
             print(*self.player4)'''#mostrar as cartas de todos
             print("RODADA: "+str(_)) #sinalizar a rodada
 
-            for player in jogadores: #cada jogador vai jogar o bgl um de cada vez
-                print("="*60) #print random
-                print(f"é a vez do jogador {player[0]}!") #print random
-                print("suas cartas:", *player[1:]) #mostrar as carta do cara bobo
-                carta = player[int(input(f"qual carta deseja jogar? {string_random}: "))] #pedir qual carta o otario quer jogar
+            for player in jogadores:
+                print("="*60)
+                print(f"é a vez do {player[0]}")
+                print(f"suas cartas: {player[1:]}")
 
-                if self.cartas[carta[0]][carta[1]] > maior: #se a carta jogada (com seu peso em questão) for maior que a maior carta jogada:
-                    maior = self.cartas[carta[0]][carta[1]] #definir que a maior carta é a atual
-                    pontuador = player #definir que o jogador que está levando é o jogador que jogou a carta
-                elif self.cartas[carta[0]][carta[1]] == maior: #se forem cartas de numero igual:
-                    maior = self.cartas[carta[0]][carta[1]] #definir que a maior ainda é a carta
-                    pontuador = ["ninguém"] #definir que ninguem pontuou
-                print(f"Carta jogada: {carta}") #print random
-                player.remove(carta) #tirar a carta q o cara jogou da mao dele
+                carta = player[int(input(f"Qual carta deseja jogar? {string_random}: "))]
+
+                print(f"carta jogada: {carta}")
+
+                if self.cartas[carta[0]][carta[1]] > maior:
+                    maior = self.cartas[carta[0]][carta[1]]
+                    pontuador = player
+                    print(f"o pontuador atual é o {pontuador} com uma carta de peso {maior}")
+                elif self.cartas[carta[0]][carta[1]] == maior:
+                    if int(player[0][-1]) % 2 == 0 and int(pontuador[0][-1]) % 2 == 0 or int(player[0][-1]) % 2 != 0 and int(pontuador[0][-1]) % 2 != 0:
+                        maior = self.cartas[carta[0]][carta[1]]
+                        pontuador = player
+                    else:
+                        maior = self.cartas[carta[0]][carta[1]]
+                        pontuador = ["ninguém"]
+
+                player.remove(carta)
             
-            print(f"{pontuador[0]} venceu esta rodada!") #print random
-
-            string_random = string_random[:-4]+")" #mudar o nmr de opções do input
-
-            if pontuador[0] != "ninguém": #se houver pontuador:
-                jogadores = jogadores[jogadores.index(pontuador):]+ jogadores[:jogadores.index(pontuador)] #redefinir a ordem de quem deve começar a proxima
-
+            print(f"{pontuador[0]} venceu esta rodada")
+            string_random = string_random[:-4]+")"
+            if pontuador[0] != "ninguém":
+                jogadores = jogadores[jogadores.index(pontuador):]+ jogadores[:jogadores.index(pontuador)]
+         
 
 truco = Truco()
 while True:
